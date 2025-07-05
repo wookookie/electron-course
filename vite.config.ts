960d0549649ca2b5ddb8a55e6d71c8b0b622a9e2
wookookie <wookookie404@gmail.com>
2025-07-05 22:17:37 +0900

    Set vite build output to dist-react directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // electron 빌드 결과와 구분하기 위함
+    outDir: "dist-react",
+  },
 });
